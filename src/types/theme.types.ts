08bb13b082d67542d63d1affbeef1ca871405ca5
진호영 <hoyoungjin9148@gmail.com>
2025-11-10 23:30:10 +0900

    style: point 컬러 생성 및 스타일 수정

--- src/types/theme.types.ts
+++ src/types/theme.types.ts
@@ -2,9 +2,11 @@ export interface Theme {
   colors: {
     bodyBg: string;
     text: string;
+    textSecondary: string;
     // 여기에 추가적인 색상들을 정의
     primary: string;
     background: string;
+    background2: string;
   };
   fonts: {
     primary: string;
